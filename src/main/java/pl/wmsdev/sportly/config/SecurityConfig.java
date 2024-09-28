d5fc9efef5ba89d3e3dee64319b05534f2207fd2
pszumanski <piotr.szumanski5@gmail.com>
2024-09-28 23:55:18 +0200

    cors

--- src/main/java/pl/wmsdev/sportly/config/SecurityConfig.java
+++ src/main/java/pl/wmsdev/sportly/config/SecurityConfig.java
@@ -1,22 +1,42 @@
 package pl.wmsdev.sportly.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	public SecurityFilterChain securityFilterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource) throws Exception {
 
 		http.csrf(csrf -> csrf.disable())
-			.cors(cors -> cors.disable())
+			.cors(cors -> cors.configurationSource(corsConfigurationSource))
 			.authorizeHttpRequests(request -> request.anyRequest().permitAll());
 
 		return http.build();
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource(@Value("${client.url:http://localhost:3000}") String frontendUrl) {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.addAllowedOrigin(frontendUrl);
+		configuration.addAllowedOrigin("http://localhost:3000");
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE", "HEAD", "OPTIONS"));
+		configuration.addAllowedHeader("*");
+		configuration.setAllowCredentials(true);
+		configuration.setMaxAge(3600L);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }
