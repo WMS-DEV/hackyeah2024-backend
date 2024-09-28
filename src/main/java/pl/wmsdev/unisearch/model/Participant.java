30ef56e0fb759fda561782332f553040adaefad3
pszumanski <piotr.szumanski5@gmail.com>
2024-09-28 12:15:22 +0200

    add participant

---
+++ src/main/java/pl/wmsdev/unisearch/model/Participant.java
@@ -0,0 +1,23 @@
+package pl.wmsdev.unisearch.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Participant {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String name;
+	private String email;
+}
